7bd191682726accf8b23b6db49640ad93368ea43
hanbit10 <chanbit10@gmail.com>
2023-10-21 11:44:55 +0200

    notfound

--- frontend/src/app/components/partials/not-found/not-found.component.ts
+++ frontend/src/app/components/partials/not-found/not-found.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent {
+  @Input()
+  visible = false;
+  //*ngIf
 
+  @Input()
+  notFoundMessage = 'Nothing Found';
+
+  @Input()
+  resetLinkText = 'Reset';
+
+  @Input()
+  resetLinkRoute = '/';
 }
