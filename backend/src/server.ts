8de449df8b961f11eac694296d47e44420457be4
hanbit10 <chanbit10@gmail.com>
2024-01-05 19:44:45 +0100

    changes server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,7 +21,11 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:4200"],
+    origin: [
+      "http://localhost:4200",
+      "https://biverse.onrender.com",
+      "https://biverse.onrender.com",
+    ],
   }),
   express.json()
 );
@@ -41,22 +45,4 @@ app.get("*", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Website served on http://localhost:" + port);
-});
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://biverse.onrender.com");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Private-Network", "true");
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
 });
